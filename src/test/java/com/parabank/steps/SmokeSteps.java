949f64f55185c8f3867bd44f5ade2c7b781f67fc
SaidounDjahid <saidoundjahid@gmail.com>
2026-07-19 17:08:36 +0200

    chore: config.properties enrichment with wait, headless, demo delay, .feature file + Steps definition file (SmokeTest.java) addition of testConfiguration loading

--- src/test/java/com/parabank/steps/SmokeSteps.java
+++ src/test/java/com/parabank/steps/SmokeSteps.java
@@ -1,14 +1,24 @@
 package com.parabank.steps;
 
+
+import com.parabank.config.Config;
 import io.cucumber.java.en.Given; //cette ligne rend l'annotation @Given disponible
+import org.testng.Assert; 
 
 
 
 public class SmokeSteps {
-
     @Given("the test framework is ready")
-    public void testFrameworkIsReady() {
-        System.out.println("The Framework is ready");
+    public void testFrameworkIsReady(){
+        System.out.println("The test framework is ready");
+    }
+    @Given("the test configuration can be loaded")
+    public void testConfigurationCanBeLoaded () {
+        String uiBaseUrl = Config.get("ui.base.url");
+        Assert.assertFalse(uiBaseUrl.isBlank(), "The UI base URL shoult not be empty."); //vérifie que ui.BaseUrl.isBlank est faux
+        //si l'URl CONTIENT du texte : isBlank() = false
+        //si l'url est vide : isBlank() = true => l'assertion échoue et le message d'erreur est affiché:
     }
 
+    
 }
--- src/test/resources/features/framework_startup.feature
+++ src/test/resources/features/framework_startup.feature
@@ -2,6 +2,8 @@ Feature: Framework Startup
 
     Scenario: Cucumber can run a simple scenario 
         Given the test framework is ready 
+        And the test configuration can be loaded
+
 
 
 
