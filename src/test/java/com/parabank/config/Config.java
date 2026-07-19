9d0a07dc44f55c4aeb2d9a5a792dc6bcc898304e
SaidounDjahid <saidoundjahid@gmail.com>
2026-07-19 16:19:54 +0200

    chore: Config.java brackekts cleaning

--- src/test/java/com/parabank/config/Config.java
+++ src/test/java/com/parabank/config/Config.java
@@ -2,33 +2,30 @@ package com.parabank.config;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.Key;
-import java.util.Properties;
+import java.util.Properties; 
 
 public final class Config {
 
-    private static final Properties PROPERTIES = new properties();
-
+    private static final Properties PROPERTIES = new Properties(); // crée un nouvel object vide storé dans PROPERTIES PROPERTIES = {}  
     static {
         try (InputStream input = Config.class.getClassLoader().getResourceAsStream("config.properties")) {
             if (input == null) {
-                throw new IllegalStateException(
+                throw new IllegalStateException( //
                     "config.properties was noud found");
 
             }
-            PROPERTIES.load(input);
+            PROPERTIES.load(input); 
         }
-        catch (IOException exception){
+        catch (IOException exception){ 
             throw new IllegalStateException(
                 "unable to read config.properties", exception);
         }
         
     }
 
-private Config (){
-}
+    private Config (){ }
 
-public static String get(String key) {
+    public static String get(String key) {
     String systemValue = System.getProperty(key);
     
     if (systemValue != null && !systemValue.isBlank()){
@@ -42,14 +39,20 @@ public static String get(String key) {
             "missing configuration value:" + key);
         
     }
+    
     return fileValue.trim();
-}
+    }
 
-public static int getInt(String key){
+
+
+    public static int getInt(String key){
     return Integer.parseInt(get(key));
-}
-public static boolean getBoolean(String key){
+    }
+
+
+    public static boolean getBoolean(String key){
     return Boolean.parseBoolean(key);
-}
+    }
+
 
 }
