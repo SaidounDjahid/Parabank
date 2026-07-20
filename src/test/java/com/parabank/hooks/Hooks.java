65745d51ae12ef686b6c59159623f49cd77b8083
SaidounDjahid <saidoundjahid@gmail.com>
2026-07-20 17:01:59 +0200

    chore: semi colon correction mising after running mvn clean test-compile

--- src/test/java/com/parabank/hooks/Hooks.java
+++ src/test/java/com/parabank/hooks/Hooks.java
@@ -18,6 +18,7 @@ public class Hooks {
 
 
     @Before("@ui") //execute les actions qui suit si le tag @ui est rencontré 
+    //avant chaque scenario portant le tag "ui" ouvre chrome 
     
 
     //ouverture de chrome et transmissions des options du chromeDriver
@@ -27,7 +28,7 @@ public class Hooks {
         if (Config.getBoolean("headless")) { //si headless égale true
             options.addArguments("--headless=new");
         }
-        options.addArguments("--window-size=1920,1080") //rajoute les détails sur la fenetre
+        options.addArguments("--window-size=1920,1080"); //rajoute les détails sur la fenetre
         context.driver = new ChromeDriver(options); // instancie un objet nouveau de type ChromeDriver et l'assigne a l'object driver de type Webdriver
 
         if (!Config.getBoolean("headless")) { // si headless est égal a false
@@ -36,6 +37,7 @@ public class Hooks {
     }
 
     @After("ui")
+    //Après chaque scénario UI, fermer Chrome.
 
     public void closeBrowser(){
         if (context.driver != null ) { //si driver contient quelque chose alors quitte le 
