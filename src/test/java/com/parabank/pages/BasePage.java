b591571d257f3c3b667aab016d782391076055b1
SaidounDjahid <saidoundjahid@gmail.com>
2026-07-21 15:04:34 +0200

    add helpers for multiple page outcomes inside base object page

--- src/test/java/com/parabank/pages/BasePage.java
+++ src/test/java/com/parabank/pages/BasePage.java
@@ -18,89 +18,81 @@ public class BasePage {
     protected final WebDriverWait wait; // sert a attendre qu'un élement soit prêt
 
     public BasePage(WebDriver driver){//constructeur de la classe BasePage => construits des objets de type BasePage
+
         this.driver = driver; //une page recoit le navigateur driver deja ouvert puis construis son attente selenium
 
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds( //le contenu de la clé wait.seconds a partir du fichier config.properties
+        this.wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(
                         Config.getInt("wait.seconds")));
-
     }
 
     protected void click(By Locator){ // procedure to click on element = Locator
-       
-       //attend jusqu'au ce que Locator soit cliquable puis continue immédiatlement
-        wait.until(ExpectedConditions.elementToBeClickable(Locator)).click(); //attends jusqu'a ce que l'élément soit pret plutot que de faire faire Thread.Sleep(3000); qui est une attente statique que ce soit lelement soit pret ou pas 
+
+        //attend jusqu'au ce que Locator soit cliquable puis continue immédiatlement
+        wait.until(
+                ExpectedConditions.elementToBeClickable(Locator))
+                .click(); //attends jusqu'a ce que l'élément soit pret plutot que de faire Thread.Sleep(3000)
     }
 
-    protected void type (By Locator, String text){ //procedure to type in text inside element = Locator
-            
-        WebElement element = wait.until(
-        ExpectedConditions.visibilityOfElementLocated(Locator));
+    protected void type(By Locator, String text){ //procedure to type in text inside element = Locator
 
-        element.clear(); //vider l'élément ie efface son ancien contenu 
+        WebElement element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(Locator));
+
+        element.clear(); //vider l'élément ie efface son ancien contenu
         element.sendKeys(text); // envoyer le texte ie ecrit le nouveau texte text
     }
 
-    protected String getText(By locator) { //to read a test for a Locator, elle lit le texte lisible d'un élément
+    protected String getText(By locator) { //to read a text for a Locator, elle lit le texte lisible d'un élément
 
         return wait.until(
                 ExpectedConditions.visibilityOfElementLocated(locator))
                 .getText()
-                .trim(); //nettoie la chaine des espaces au début et des espaces a la   
+                .trim(); //nettoie la chaine des espaces au début et a la fin
     }
-
-    // protected void selectByText(By locator, String text) { //this method select the option by the visible text to the user par example <select id="type">
-    // //<option value="0">CHECKING</option>
-    // //<option value="1">SAVINGS</option>
-    // //</select> 
-    // //selectByText(By.id("type"), "SAVINGS"); => selenium cherche l'option visible a l'utilisateur i.e Savings
-
-    // //
-
-    //     WebElement element = wait.until(
-    //             ExpectedConditions.elementToBeClickable(locator));
-
-    //     new Select(element).selectByVisibleText(text);
-    // }
-
 
     protected void selectByText(By locator, String text) {
 
-    wait.until(driver -> {
+        /*
+         * On attend que l'option demandé soit disponible dans le select.
+         * C'est utile car certaines options ParaBank peuvent arriver après le chargement de la page.
+         */
+        wait.until(driver -> {
+
+            WebElement selectElement =
+                    driver.findElement(locator);
+
+            Select select =
+                    new Select(selectElement);
+
+            return select.getOptions()
+                    .stream()
+                    .anyMatch(option ->
+                            option.getText()
+                                    .trim()
+                                    .equals(text));
+        });
 
         WebElement selectElement =
                 driver.findElement(locator);
 
-        Select select =
-                new Select(selectElement);
-
-        return select.getOptions()
-                .stream()
-                .anyMatch(option ->
-                        option.getText()
-                                .trim()
-                                .equals(text));
-    });
-
-    WebElement selectElement =
-            driver.findElement(locator);
-
-    new Select(selectElement)
-            .selectByVisibleText(text);
-
+        new Select(selectElement)
+                .selectByVisibleText(text);
     }
 
     protected void selectByValue(By locator, String value) {//cette méthode selectionne l'option selon la valeur technique contenu dans l'attribut technique html value
-    //<select id="type">
-    //<option value="0">CHECKING</option>
-    //<option value="1">SAVINGS</option>
-    //</select>
-    // selectByValue(By.id("type"), "1"); Selenium cherche <option value="1"> puis sélectionne SAVINGS
-
+        //<select id="type">
+        //<option value="0">CHECKING</option>
+        //<option value="1">SAVINGS</option>
+        //</select>
+        // selectByValue(By.id("type"), "1"); Selenium cherche <option value="1"> puis sélectionne SAVINGS
 
         WebElement element = wait.until(
                 ExpectedConditions.elementToBeClickable(locator));
 
-        new Select(element).selectByValue(value);
+        new Select(element)
+                .selectByValue(value);
     }
 
     protected boolean isVisible(By locator) { //attend qu'un élement devient visible
@@ -110,6 +102,51 @@ public class BasePage {
                 .isDisplayed();
     }
 
-    //les procedures de la page BasePage seront utilisés par toutes les pages du site web (LoginPage,OpenNewAccountPage,AccountOverviewPage,BillPayPage), elle devront donc utiliser les méthodes de la classe BasePage, il faut alors que les méthodes soient en protected afin que les classes enfants puisse les utiliser, 
+    protected boolean exists(By locator) { //verifie directement si au moins un element visible existe sur la page
 
+        /*
+         * findElements ne provoque pas d'erreur quand l'element n'existe pas.
+         * Il retourne simplement une liste vide.
+         *
+         * On vérifie aussi isDisplayed car un element peut etre présent
+         * dans le HTML mais caché a l'utilisateur.
+         */
+        return driver.findElements(locator)
+                .stream()
+                .anyMatch(WebElement::isDisplayed);
+    }
+
+    protected void waitForAny(By... locators) { //attend qu'au moins un des differents résultats possible apparaisse
+
+        /*
+         * By... permet de passer plusieurs locators à la méthode.
+         *
+         * Exemple:
+         * waitForAny(accountServicesTitle, loginErrorMessage);
+         *
+         * L'attente s'arrete dès qu'un des éléments est visible.
+         */
+        wait.until(currentDriver -> {
+
+            for (By locator : locators) {
+
+                boolean elementIsVisible =
+                        currentDriver.findElements(locator)
+                                .stream()
+                                .anyMatch(WebElement::isDisplayed);
+
+                if (elementIsVisible) {
+
+                    return true;
+                }
+            }
+
+            return false;
+        });
+    }
+
+    //les procedures de la page BasePage seront utilisés par toutes les pages du site web
+    //(LoginPage, CustomerLookupPage, OpenNewAccountPage, AccountOverviewPage, BillPayPage)
+    //elles devront donc utiliser les méthodes de la classe BasePage, il faut alors que les méthodes
+    //soient en protected afin que les classes enfants puissent les utiliser
 }
